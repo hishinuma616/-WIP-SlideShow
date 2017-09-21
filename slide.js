d130c223c7cbba4a701e097148af5852454ea6ca
hishinuma616 <dnbwoodleaves3616@gmail.com>
2017-09-22 07:19:17 +0900

    スタイル調整

--- slide.js
+++ slide.js
@@ -12,66 +12,16 @@ myNowCnt = 0;		// 現在表示している配列番号
 myflg = 0;		// どっちを表示して、どっちを消すかのフラグ
 var oya = document.getElementById('js-slide-index');
 
-function myChange(check){	// スライドショーメイン関数
-  if(check==1){ //加算式
-    if(myNowCnt<myImage.length-1){ // 次の配列番号
-      myNowCnt = myNowCnt+1;
-    }else{
-      myNowCnt = 0;
-    }
-  }else if(check==0){ //減少式
-    if(myNowCnt==0){ // 次の配列番号
-      myNowCnt = myImage.length-1;
-    }else{
-      myNowCnt = myNowCnt-1;
-    }
-  }
-
-  myflg = (myflg==0) ? 1 : 0;						// 表示・非表示フラグ反転
-  if (myflg == 0){
-    document.getElementById("idshow1").src = myImage[myNowCnt];		// 次の画像をセットする
-    document.getElementById("idshow1").className = "fadein";		// フェードイン
-    document.getElementById("idshow2").className = "fadeout";	// フェードアウト
-    indexCheck();
-
-  }else{
-    document.getElementById("idshow2").src = myImage[myNowCnt];		// 次の画像をセットする
-    document.getElementById("idshow1").className = "fadeout";	// フェードアウト
-    document.getElementById("idshow2").className = "fadein";		// フェードイン
-    indexCheck();
-  }
-}
-
-function indexCheck(){
-  for(j=0; j<=myImage.length-1 ;j++){
-    if(j!=myNowCnt){
-      //  document.getElementById(j).className = "slide-index-icon"; //インデックスの色を変える
-      document.getElementById("thumbnail"+j).className = "slide-thumbnail";
-    }else{
-      //document.getElementById(j).className = "slide-index-icon slide-index-active"; //インデックスの色を変える
-      document.getElementById("thumbnail"+j).className = "slide-thumbnail slide-thumbnail-active";
-    }
-  }
-}
-
-function indexChange(point){
-  if(point == 0){
-    myNowCnt = myImage.length-1-point;
-  }else{
-    myNowCnt = point-1;
-  }
-  myChange(1);
-}
 
 function out(){
-  timerID=setInterval("myChange(1)",2000);	// 2秒周期に画像を更新する
+  timerID=setInterval("slideAnimation()",2000);	// 2秒周期に画像を更新する
 }
 
 function outStop(){
   clearInterval(timerID);
 }
 
-function listAdd(){
+function listChange(){
   var elemLi = document.createElement('li');  // li要素生成
   elemLi.setAttribute("id","hoge");
   elemLi.setAttribute("class","slide-index-icon");
@@ -90,9 +40,9 @@ function listAdd(){
 
 function slideAnimation(){
   oya.style.transition="0.5s";
-  oya.style.left = "-78px";
+  oya.style.left = "-368px";
 
-  window.setTimeout("listAdd()", "550");
+  window.setTimeout("listChange()", "550");
 }
 
 // ----------サムネイルの処理 ここから▼----------
